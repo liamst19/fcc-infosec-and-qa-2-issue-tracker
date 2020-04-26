37ba222f56979ac1cebf96c95d32766831fa4181
Glitch (chiseled-bald-tile) <none>
2020-04-26 10:51:21 +0000

    🌞🤘 Checkpoint ./routes/api.js:15054522/4437

--- routes/api.js
+++ routes/api.js
@@ -1,63 +1,62 @@
 /*
-*
-*
-*       Complete the API routing below
-*
-*
-*/
+ *
+ *
+ *       Complete the API routing below
+ *
+ *
+ */
 
-'use strict';
+"use strict";
 
-var expect = require('chai').expect;
-var MongoClient = require('mongodb');
-var ObjectId = require('mongodb').ObjectID;
-var bodyParser = require('body-parser');
+var expect = require("chai").expect;
+var MongoClient = require("mongodb");
+var ObjectId = require("mongodb").ObjectID;
+var bodyParser = require("body-parser");
 
-const CONNECTION_STRING = process.env.DB; 
-
-module.exports = function (app) {
-  
+module.exports = function(app) {
   app.use(bodyParser.text());
-  
-  MongoClient.connect(CONNECTION_STRING, function(err, db) {
-    if(err) console.log('Database error: ' + err);
-    console.log('connected to database')
-    
-    app.route('/api/issues/:project')  
-      .get(function (req, res){
+
+  MongoClient.connect(process.env.DB, (err, db) => {
+    if (err) console.log("Database error: " + err);
+    console.log("connected to database");
+
+    app
+      .route("/api/issues/:project")
+      .get(function(req, res) {
         var project = req.params.project;
+        console.log("GET", project);
       })
 
-      .post(function (req, res){
-        
+      .post(function(req, res) {
         var project = req.params.project;
-        console.log('posting', project)
-        db.collection('issues').insertOne({
+        const issue = {
           project,
           issue_title: req.body.issue_title,
           issue_text: req.body.issue_text,
           created_by: req.body.created_by,
           assigned_to: req.body.assigned_to,
           status_text: req.body.status_text
-        }, (err, doc) => {
-          if(err){
-            console.log('error', err);
-            res.json({ error: 'something went wrong'});
-          } else{
-            console.log('success', doc)
+        };
+        console.log("POST", { project, issue });
+        db.collection("issues").insertOne(issue, (err, doc) => {
+          if (err) {
+            console.log("error", err);
+            res.json({ error: "something went wrong" });
+          } else {
+            console.log("success", doc);
             res.json(doc);
           }
         });
       })
 
-      .put(function (req, res){
+      .put(function(req, res) {
         var project = req.params.project;
-
+        console.log("PUT", project);
       })
 
-      .delete(function (req, res){
+      .delete(function(req, res) {
         var project = req.params.project;
-
+        console.log("DELETE", project);
       });
-    });
+  });
 };
