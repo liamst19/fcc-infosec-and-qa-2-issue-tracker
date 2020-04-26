963c025c8952e44dc27a18bdafa3efd8cfd23ed0
Glitch (chiseled-bald-tile) <none>
2020-04-26 09:09:20 +0000

    🌵🌲 Checkpoint ./routes/api.js:15054522/1026

--- routes/api.js
+++ routes/api.js
@@ -11,20 +11,30 @@
 var expect = require('chai').expect;
 var MongoClient = require('mongodb');
 var ObjectId = require('mongodb').ObjectID;
+var bodyParser = require('body-parser');
 
 const CONNECTION_STRING = process.env.DB; //MongoClient.connect(CONNECTION_STRING, function(err, db) {});
 
 module.exports = function (app) {
-
-  app.route('/api/issues/:project')
   
+  app.use(bodyParser.text());
+  
+  app.route('/api/issues/:project')  
     .get(function (req, res){
       var project = req.params.project;
-      
     })
     
     .post(function (req, res){
       var project = req.params.project;
+      const issue = {
+        project,
+        issue_title: req.body.issue_title,
+        issue_text: req.body.issue_text,
+        created_by: req.body.created_by,
+        assigned_to: req.body.assigned_to,
+        status_text: req.body.status_text
+      }
+      console.log(issue)
       
     })
     
