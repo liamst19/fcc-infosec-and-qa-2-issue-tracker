76ff4c121b923487bfe0834a3b1f04c098eb9bfb
Glitch (chiseled-bald-tile) <none>
2020-04-26 09:21:18 +0000

    🔔✍️ Checkpoint ./routes/api.js:15054522/513

--- routes/api.js
+++ routes/api.js
@@ -13,39 +13,42 @@ var MongoClient = require('mongodb');
 var ObjectId = require('mongodb').ObjectID;
 var bodyParser = require('body-parser');
 
-const CONNECTION_STRING = process.env.DB; //MongoClient.connect(CONNECTION_STRING, function(err, db) {});
+const CONNECTION_STRING = process.env.DB; 
+
 
 module.exports = function (app) {
   
   app.use(bodyParser.text());
-  
-  app.route('/api/issues/:project')  
-    .get(function (req, res){
-      var project = req.params.project;
-    })
+  MongoClient.connect(CONNECTION_STRING, function(err, db) {
+    if(err) console.log('Database error: ' + err);
     
-    .post(function (req, res){
-      var project = req.params.project;
-      const issue = {
-        project,
-        issue_title: req.body.issue_title,
-        issue_text: req.body.issue_text,
-        created_by: req.body.created_by,
-        assigned_to: req.body.assigned_to,
-        status_text: req.body.status_text
-      }
-      console.log(issue)
-      
-    })
-    
-    .put(function (req, res){
-      var project = req.params.project;
-      
-    })
-    
-    .delete(function (req, res){
-      var project = req.params.project;
-      
+    app.route('/api/issues/:project')  
+      .get(function (req, res){
+        var project = req.params.project;
+      })
+
+      .post(function (req, res){
+        var project = req.params.project;
+        const issue = {
+          project,
+          issue_title: req.body.issue_title,
+          issue_text: req.body.issue_text,
+          created_by: req.body.created_by,
+          assigned_to: req.body.assigned_to,
+          status_text: req.body.status_text
+        }
+        console.log(issue)
+
+      })
+
+      .put(function (req, res){
+        var project = req.params.project;
+
+      })
+
+      .delete(function (req, res){
+        var project = req.params.project;
+
+      });
     });
-    
 };
